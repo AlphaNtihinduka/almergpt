5e51dee2392c5e7509a35d6901f6736e26a57c41
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-06-19 22:46:03 +0300

    feat: add appwrite client configuration for admin access

---
+++ config/appwrite.ts
@@ -0,0 +1,26 @@
+import { Account, Client, Databases, Users, Storage } from 'node-appwrite';
+
+const createAdminClient = async () => {
+  const client = new Client();
+  client
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
+    .setKey(process.env.NEXT_PUBLIC_APPWRITE_SECRET_KEY!);
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get users() {
+      return new Users(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+  };
+};
+
+const appwriteAdmin = createAdminClient();
+export default appwriteAdmin;
