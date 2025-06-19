7b39dfd9638d9856bd5b0c76c7b70739ca652316
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-06-20 00:02:51 +0300

    refactor: clean up commented code and improve formatting in MobileSidebar component

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,7 +4,7 @@ import { Menu } from "lucide-react"
 import { Button } from "./ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 import SideBar from "./sideBar"
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 
 const MobileSidebar = () => {
     // const [isMounted, setIsMounted] = useState(false);
@@ -17,19 +17,19 @@ const MobileSidebar = () => {
     //     return null;
     // }
     return (
-        <Sheet> 
-         <SheetTrigger>
-            <div>
-                <Button variant="ghost" size="icon" className="md:hidden">
-                    <Menu/>
-                </Button>
-            </div> 
-         </SheetTrigger>
+        <Sheet>
+            <SheetTrigger>
+                <div>
+                    <Button variant="ghost" size="icon" className="md:hidden">
+                        <Menu />
+                    </Button>
+                </div>
+            </SheetTrigger>
             <SheetContent side="left" className="p-0">
-                <SideBar/>
+                <SideBar />
             </SheetContent>
         </Sheet>
-       
+
     )
 }
 
