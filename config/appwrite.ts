6b8cd2cf296f72d198eddcae12ff8aaa9708c5ba
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-06-20 09:31:48 +0300

    fix: correct environment variable reference for Appwrite secret key

--- config/appwrite.ts
+++ config/appwrite.ts
@@ -5,7 +5,7 @@ const createAdminClient = async () => {
   client
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
-    .setKey(process.env.NEXT_PUBLIC_APPWRITE_SECRET_KEY!);
+    .setKey(process.env.APPWRITE_SECRET_KEY!);
   return {
     get account() {
       return new Account(client);
